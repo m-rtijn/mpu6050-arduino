ca5982bbeb298f112fbcc9cca1e607029b9a06e6
Tijndagamer <tijndagamer25@gmail.com>
2016-12-09 20:05:48 +0100

    Make pre-defined ranges public Define readAccelAxis and readGyroAxis methods

--- MPU6050.h
+++ MPU6050.h
@@ -34,19 +34,20 @@ class MPU6050 {
         float readGyroY();
         float readGyroZ();
 
-        uint8_t readAccelRange();
-        uint8_t readGyroRange();
+        uint8_t getAccelRange();
+        uint8_t getGyroRange();
 
-        // Scale modifiers
-        const float ACCEL_SCALE_MODIFIER_2G = 16384.0;
-        const float ACCEL_SCALE_MODIFIER_4G = 8192.0;
-        const float ACCEL_SCALE_MODIFIER_8G = 4096.0;
-        const float ACCEL_SCALE_MODIFIER_16G = 2048.0;
+        // Ranges
+        const uint8_t ACCEL_RANGE_2G = 0x00;
+        const uint8_t ACCEL_RANGE_4G = 0x08;
+        const uint8_t ACCEL_RANGE_8G = 0x10;
+        const uint8_t ACCEL_RANGE_16G = 0x18;
 
-        const float GYRO_SCALE_MODIFIER_250DEG = 131.0;
-        const float GYRO_SCALE_MODIFIER_500DEG = 65.5;
-        const float GYRO_SCALE_MODIFIER_1000DEG = 32.8;
-        const float GYRO_SCALE_MODIFIER_2000DEG = 16.4;
+        const uint8_t GYRO_RANGE_250DEG = 0x00;
+        const uint8_t GYRO_RANGE_500DEG = 0x08;
+        const uint8_t GYRO_RANGE_1000DEG = 0x10;
+        const uint8_t GYRO_RANGE_2000DEG = 0x18;
+
     private:
         // Constants
         const float GRAVITY_MS2 = 9.80665;
@@ -66,21 +67,23 @@ class MPU6050 {
         const uint8_t GYRO_ZOUT0 = 0x48;
         const uint8_t TEMP_OUT0 = 0x41;
 
-        // Ranges
-        const uint8_t ACCEL_RANGE_2G = 0x00;
-        const uint8_t ACCEL_RANGE_4G = 0x08;
-        const uint8_t ACCEL_RANGE_8G = 0x10;
-        const uint8_t ACCEL_RANGE_16G = 0x18;
+        // Scale modifiers
+        const float ACCEL_SCALE_MODIFIER_2G = 16384.0;
+        const float ACCEL_SCALE_MODIFIER_4G = 8192.0;
+        const float ACCEL_SCALE_MODIFIER_8G = 4096.0;
+        const float ACCEL_SCALE_MODIFIER_16G = 2048.0;
 
-        const uint8_t GYRO_RANGE_250DEG = 0x00;
-        const uint8_t GYRO_RANGE_500DEG = 0x08;
-        const uint8_t GYRO_RANGE_1000DEG = 0x10;
-        const uint8_t GYRO_RANGE_2000DEG = 0x18;
+        const float GYRO_SCALE_MODIFIER_250DEG = 131.0;
+        const float GYRO_SCALE_MODIFIER_500DEG = 65.5;
+        const float GYRO_SCALE_MODIFIER_1000DEG = 32.8;
+        const float GYRO_SCALE_MODIFIER_2000DEG = 16.4;
 
         // Internal methods
         int16_t read_i2c_word(uint8_t register_msb);
         int8_t read_i2c_byte(uint8_t i2c_register);
         void write_i2c_byte(uint8_t i2c_register, uint8_t cmd);
+        float readAccelAxis(char axis);
+        float readGyroAxis(char axis);
 };
 
 #endif
